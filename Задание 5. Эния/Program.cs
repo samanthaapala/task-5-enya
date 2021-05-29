84950376b247dc1f1dfc70f71fa03a0f03251f15
Samantha Apala <s.apala@yandex.ru>
2021-05-29 16:38:02 +0300

    Добавьте файлы проекта.

---
+++ Задание 5. Эния/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5.Эния
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Для обработки панелей сульфидом тория необходимо {a * b * c * 2} нанограмм");
+            Console.ReadKey();
+        }
+    }
+}
